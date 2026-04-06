8e96466a044fac229f5edf5cb5f2e38819f45326
korsh624 <korsh624@gmail.com>
2026-04-06 18:24:05 +0300

    add test 2026

---
+++ Software/test2026.py
@@ -0,0 +1,12 @@
+import time
+
+from promtools import Manipulator
+man1=Manipulator('192.168.10.18',8888,'g')
+man1.toPoint(200,0,200,0,0)
+time.sleep(2)
+man1.toPoint(200,0,3,0,0)
+time.sleep(2)
+man1.toPoint(200,0,10,0,1)
+time.sleep(2)
+man1.toPoint(200,0,200,0,1)
+
